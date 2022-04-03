de0f49a024963cbd56b287dde94256a964dff0c1
gbueno <gabrielb77@gmail.com>
2022-04-03 11:33:36 -0300

    test 5

--- test_flasktest.py
+++ test_flasktest.py
@@ -6,22 +6,6 @@ from urllib.parse import urlparse
 import sqlite3
 
 
-
-# content of test_sample.py
-def inc(x):
-  return x + 1
-
-
-def test_answer():
-  assert inc(3) == 4
-
-# test.py
-def test_always_passes():
-  assert True
-
-def test_always_fails():
-  assert False
-
 def test_GetRandom():
   import flasktest
 
